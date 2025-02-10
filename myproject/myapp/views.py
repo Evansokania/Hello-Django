9ed104f7d914d3316cba16a0ffd0a45c654dd3fa
Evansokania <evans1.okania@gmail.com>
2025-02-10 10:04:47 +0300

    form implementation

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Employee
-
+from .forms import EmployeeForm
 
 # Create your views here.
 from django.http import HttpResponse
@@ -11,3 +11,15 @@ def home(request):
 def employee_list(request):
     employees = Employee.objects.all()
     return render(request, "myapp/index.html", {"employees": employees})
+
+
+def add_employee(request):
+    if request.method == "POST":
+        form = EmployeeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("employee-list")
+    else:
+        form = EmployeeForm()
+
+    return render(request, "myapp/add_employee.html", {"form": form})
