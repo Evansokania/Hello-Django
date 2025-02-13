233220edc1de71dff269ed476f90d8ad20d1e187
Evansokania <evans1.okania@gmail.com>
2025-02-13 16:26:43 +0300

    admin

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,8 +1,33 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser,BaseUserManager
+
+
+class CustomUserManager(BaseUserManager):
+    """Manager for CustomUser model"""
+
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_admin", True)
+
+        return self.create_user(username, email, password, **extra_fields)
 
 class CustomUser(AbstractUser):
-     is_admin = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    
+    objects = CustomUserManager()  # Assign the custom manager
+
+    def __str__(self):
+        return self.username
 
 class Employee(models.Model):
     first_name = models.CharField(max_length=100)
@@ -13,5 +38,3 @@ class Employee(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
-
-
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
 from .forms import RegisterForm, EmployeeForm
 from django.http import HttpResponseForbidden
-from .models import Employee
+from .models import Employee,CustomUser
 
 # Create your views here.
 from django.http import HttpResponse
@@ -29,11 +29,16 @@ def add_employee(request):
     return render(request, "myapp/add_employee.html", {"form": form})
 
 
+
 def register(request):
     if request.method == "POST":
         form = RegisterForm(request.POST)
         if form.is_valid():
-            user = form.save()
+            user = CustomUser.objects.create_user(
+                username=form.cleaned_data["username"],
+                email=form.cleaned_data["email"],
+                password=form.cleaned_data["password1"]
+            )
             login(request, user)
             return redirect("employee-list")
     else:
